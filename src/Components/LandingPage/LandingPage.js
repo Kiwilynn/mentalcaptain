313f0ac20750807e521d1c0f67c5c207194a497c
Kiwilynn <kasll@hotmail.com>
2021-08-28 07:56:54 +0200

    playing around with landingpage background and colors

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -6,7 +6,15 @@ const LandingPage = () => {
     return (
         <>
         <section className={Styles.sectiontest}>
-            <div className={Styles.div1test}></div>
+            <div className={Styles.div1test}>
+                <header>
+                    <h1>Lorem ipsum dolor sit amet.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum voluptatum cum fuga accusamus distinctio? Impedit, beatae quos quidem eum commodi ea tempore inventore omnis quia eaque optio sit animi consequatur corporis voluptas, in ducimus iste veritatis natus fugiat possimus sapiente perferendis similique? Magnam fugit perferendis est, tempora atque ea aliquid.</p>
+                    <p>Vero eaque a non enim quis eveniet ab repellendus consectetur, veritatis minima doloribus cumque et ducimus reprehenderit natus. Nulla nobis neque sit, accusamus facilis laborum alias deleniti, earum quia dolorum doloribus perspiciatis voluptas culpa cupiditate aliquid? Sint, similique. Incidunt dolor similique, ab minima sunt praesentium libero. Omnis itaque veritatis laboriosam?</p>
+                    <p>Voluptas ad atque eius veniam, adipisci tenetur id consequuntur delectus voluptatum perferendis perspiciatis incidunt, tempora nesciunt qui esse mollitia! Nostrum quo iure maxime repellat quidem ipsum, quis voluptates libero ab mollitia distinctio quae necessitatibus recusandae quam alias id dolor architecto excepturi, dolorem dolorum natus illum voluptate ea adipisci? Consequuntur, rerum?</p>
+                    <p>Magni quia dolorum non iste minima deleniti accusantium beatae laboriosam, reprehenderit sequi architecto nam mollitia harum maiores dolorem necessitatibus, tenetur veniam enim libero aperiam. Id dolore autem nulla accusantium, est corrupti consequatur commodi harum debitis, laborum architecto doloribus atque quae ratione minus sit sapiente in itaque aliquam cum quibusdam ullam?</p>
+                </header>
+            </div>
             <div className={Styles.div3test}></div>
             <div className={Styles.div2test}></div>
         </section>
